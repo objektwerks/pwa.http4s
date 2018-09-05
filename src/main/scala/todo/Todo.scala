d147ecb8cce87ed4d23e7bfe02ef710ee85026f7
objektwerks <objektwerks@runbox.com>
2018-09-04 20:25:53 -0400

    added put and delete to rest http service

--- src/main/scala/todo/Todo.scala
+++ src/main/scala/todo/Todo.scala
@@ -2,10 +2,12 @@ package todo
 
 import cats.effect.IO
 import io.circe.generic.auto._
-import org.http4s.circe.jsonOf
+import org.http4s.circe._
 
 case class Todo(id: Int, task: String, assigned: String, completed: Option[String])
 
 object Todo {
   implicit val todoDecoder = jsonOf[IO, Todo]
+  implicit val todoEncoder = jsonEncoderOf[IO, Todo]
+  implicit val todoIdEncoder = jsonEncoderOf[IO, Int]
 }
--- src/main/scala/todo/TodoService.scala
+++ src/main/scala/todo/TodoService.scala
@@ -9,8 +9,20 @@ import org.http4s.dsl.impl.Root
 import org.http4s.dsl.io._
 
 case class TodoService(repository: TodoRepository) {
+  import Todo._
+
   val instance = HttpService[IO] {
     case GET -> Root / "todos" => Ok(repository.select.asJson)
-    case POST -> Root / "todo" => Ok()
+    case request @ POST -> Root / "todo" =>
+      for {
+        todo <- request.as[Todo]
+        result <- Ok(repository.insert(todo).asJson)
+      } yield result
+    case request @ PUT -> Root / "todo" =>
+      for {
+        todo <- request.as[Todo]
+        result <- Ok(repository.update(todo).asJson)
+      } yield result
+    case DELETE -> Root / "todo" / IntVar(id) => Ok(repository.delete(id))
   }
 }
