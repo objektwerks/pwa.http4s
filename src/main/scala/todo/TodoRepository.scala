6a778ac0e39df19191890a7fb2fbbfeabfaca7c7
objektwerks <objektwerks@runbox.com>
2018-09-06 16:14:10 -0400

    resolved prepared statement mystery

--- src/main/scala/todo/TodoRepository.scala
+++ src/main/scala/todo/TodoRepository.scala
@@ -1,5 +1,7 @@
 package todo
 
+import java.sql.Timestamp
+
 import cats.effect._
 import doobie._
 import doobie.implicits._
@@ -9,7 +11,11 @@ import scala.io.Source
 case class TodoRepositoryConfig(schema: String, driver: String, url: String, user: String, password: String)
 
 case class TodoRepository(config: TodoRepositoryConfig, init: Boolean = false) {
-  private val xa = Transactor.fromDriverManager[IO](config.driver, config.url, config.user, config.password)
+  val xa = Transactor.fromDriverManager[IO](config.driver, config.url, config.user, config.password)
+  val selectPS = sql"select * from todo".query[Todo]
+  val insertPS = Update[(String, Timestamp, Option[Timestamp])]("insert into todo(task, assigned, completed) values (?, ?, ?)")
+  val updatePS = Update[(String, Option[Timestamp], Int)]("update todo set task = ?, completed = ? where id = ?")
+  val deletePS = Update[Int]("delete from todo where id = ?")
 
   if (init) init(config.schema)
 
@@ -18,27 +24,16 @@ case class TodoRepository(config: TodoRepositoryConfig, init: Boolean = false) {
     Fragment.const(schema).update.run.transact(xa).unsafeRunSync
   }
 
-  def select: List[Todo] = {
-    val select = sql"select * from todo".query[Todo]
-    select.to[List].transact(xa).unsafeRunSync
-  }
+  def select: List[Todo] = selectPS.to[List].transact(xa).unsafeRunSync
 
   def insert(todo: Todo): Int = {
     val insert = for {
-      id <- sql"insert into todo(task, assigned, completed) values (${todo.task}, ${todo.assigned}, ${todo.completed})"
-        .update
-        .withUniqueGeneratedKeys[Int]("id")
+      id <- insertPS.toUpdate0((todo.task, todo.assigned, todo.completed)).withUniqueGeneratedKeys[Int]("id")
     } yield id
     insert.transact(xa).unsafeRunSync
   }
 
-  def update(todo: Todo): Int = {
-    val update = sql"update todo set task = ${todo.task}, completed = ${todo.completed} where id = ${todo.id}".update
-    update.run.transact(xa).unsafeRunSync
-  }
+  def update(todo: Todo): Int = updatePS.toUpdate0((todo.task, todo.completed, todo.id)).run.transact(xa).unsafeRunSync
 
-  def delete(id: Int): Int = {
-    val delete = sql"delete from todo where id = $id".update
-    delete.run.transact(xa).unsafeRunSync
-  }
+  def delete(id: Int): Int = deletePS.toUpdate0(id).run.transact(xa).unsafeRunSync
 }
