005e1b723a4a89cb9ed2c1f2eef374b94e220e8a
objektwerks <objektwerks@runbox.com>
2018-09-07 03:54:58 -0400

    renamed prepared queries

--- src/main/scala/todo/TodoRepository.scala
+++ src/main/scala/todo/TodoRepository.scala
@@ -12,10 +12,10 @@ case class TodoRepositoryConfig(schema: String, driver: String, url: String, use
 
 case class TodoRepository(config: TodoRepositoryConfig, init: Boolean = false) {
   val xa = Transactor.fromDriverManager[IO](config.driver, config.url, config.user, config.password)
-  val qSelect = sql"select * from todo".query[Todo]
-  val uInsert = Update[(String, Timestamp, Option[Timestamp])]("insert into todo(task, assigned, completed) values (?, ?, ?)")
-  val uUpdate = Update[(String, Option[Timestamp], Int)]("update todo set task = ?, completed = ? where id = ?")
-  val uDelete = Update[Int]("delete from todo where id = ?")
+  val selectTodos = sql"select * from todo".query[Todo]
+  val insertTodo = Update[(String, Timestamp, Option[Timestamp])]("insert into todo(task, assigned, completed) values (?, ?, ?)")
+  val updateTodo = Update[(String, Option[Timestamp], Int)]("update todo set task = ?, completed = ? where id = ?")
+  val deleteTodo = Update[Int]("delete from todo where id = ?")
 
   if (init) init(config.schema)
 
@@ -24,16 +24,16 @@ case class TodoRepository(config: TodoRepositoryConfig, init: Boolean = false) {
     Fragment.const(schema).update.run.transact(xa).unsafeRunSync
   }
 
-  def select: List[Todo] = qSelect.to[List].transact(xa).unsafeRunSync
+  def select: List[Todo] = selectTodos.to[List].transact(xa).unsafeRunSync
 
   def insert(todo: Todo): Int = {
     val insert = for {
-      id <- uInsert.toUpdate0((todo.task, todo.assigned, todo.completed)).withUniqueGeneratedKeys[Int]("id")
+      id <- insertTodo.toUpdate0((todo.task, todo.assigned, todo.completed)).withUniqueGeneratedKeys[Int]("id")
     } yield id
     insert.transact(xa).unsafeRunSync
   }
 
-  def update(todo: Todo): Int = uUpdate.toUpdate0((todo.task, todo.completed, todo.id)).run.transact(xa).unsafeRunSync
+  def update(todo: Todo): Int = updateTodo.toUpdate0((todo.task, todo.completed, todo.id)).run.transact(xa).unsafeRunSync
 
-  def delete(id: Int): Int = uDelete.toUpdate0(id).run.transact(xa).unsafeRunSync
+  def delete(id: Int): Int = deleteTodo.toUpdate0(id).run.transact(xa).unsafeRunSync
 }
