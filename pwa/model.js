02fa6d2a97698b4ee3c8261e98531bb751d89143
objektwerks <objektwerks@runbox.com>
2018-09-14 17:28:46 -0400

    added dummy data to model.js

--- pwa/model.js
+++ pwa/model.js
@@ -1,4 +1,27 @@
 export default () => {
+    class Todo { 
+        constructor(id, todo, opened = new Date().getTime, closed = 0) { 
+           this.id = id; 
+           this.todo = todo;
+           this.opened = opened;
+           this.closed = closed;
+        } 
+     }
+
+    let todos = new Map(); // Temporary Http fetch.
+
+    todos.set(1, new Todo(1, 'Wash car.'));
+    todos.set(2, new Todo(2, 'Dry car.'));
+    todos.set(3, new Todo(3, 'Wax car.'));
+    let ul = document.getElementById('todo-list');
+    for (let [id, todo] of todos) {
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(todo.todo));
+        li.setAttribute('id', id); 
+        li.setAttribute('class', 'w3-hover-light-gray')
+        ul.appendChild(li);
+    }
+    
     document.getElementById('add-todo').addEventListener('click', event => {
         console.log('add-todo: click...', event);
     });
