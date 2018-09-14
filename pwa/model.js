cd0a70e14ac7c1545af88d37737fc4d420c9e03b
objektwerks <objektwerks@runbox.com>
2018-09-14 14:22:14 -0400

    added event listeners to model.js

--- pwa/model.js
+++ pwa/model.js
@@ -1,3 +1,17 @@
 export default () => {
-    let model = new Map();
+    const model = new Map();
+
+    console.log('model:', model);
+
+    document.getElementById('add-todo').addEventListener('click', event => {
+        console.log('add-todo: click...', event);
+    });
+
+    document.getElementById('remove-todo').addEventListener('click', event => {
+        console.log('remove-todo: click...', event);
+    });
+
+    document.getElementById('todo-list').addEventListener('click', event => {
+        console.log('todo-list: click...', event.target.id, event.target.textContent);
+    });
 };
