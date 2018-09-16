fbda56a9da5c90909c75eb85ffbadbdb69746ccf
objektwerks <objektwerks@runbox.com>
2018-09-16 14:25:56 -0400

    edited model.js

--- pwa/model.js
+++ pwa/model.js
@@ -1,6 +1,8 @@
 export default () => {
+    let todos = new Map();
+
     class Todo { 
-        constructor(id, todo, opened = new Date().toISOString(), closed = "") {
+        constructor(id = 0, todo, opened = new Date().toLocaleDateString(), closed = "") {
            this.id = id; 
            this.todo = todo;
            this.opened = opened;
@@ -8,17 +10,21 @@ export default () => {
         } 
      }
 
-    let todos = new Map(); // Temporary!
-    todos.set('1', new Todo(1, 'Wash car.'))
+    function setTodoList(todos) {
+        let ul = document.getElementById('todo-list');
+        ul.innerHTML = '';
+        for (let [id, todo] of todos) {
+            let li = document.createElement('li');
+            li.appendChild(document.createTextNode(todo.todo));
+            li.setAttribute('id', id); 
+            li.setAttribute('class', 'w3-hover-light-gray')
+            ul.appendChild(li);
+        }    
+    }
 
-    let ul = document.getElementById('todo-list');
-    ul.innerHTML = '';
-    for (let [id, todo] of todos) {
-        let li = document.createElement('li');
-        li.appendChild(document.createTextNode(todo.todo));
-        li.setAttribute('id', id); 
-        li.setAttribute('class', 'w3-hover-light-gray')
-        ul.appendChild(li);
+    function getSelectedTodo() {
+        let id = document.getElementById('todo-id').value;
+        return todos.get(id);
     }
 
     function clearTodoFields() {
@@ -38,27 +44,32 @@ export default () => {
     
     document.getElementById('todo-list').addEventListener('click', event => {
         console.log('todo-list: click...', event.target.id, event.target.textContent);
-        document.getElementById('remove-todo').disabled = false;
         setTodoFields(event.target.id);
+        document.getElementById('remove-todo').disabled = false;
     });
 
     document.getElementById('add-todo').addEventListener('click', event => {
         console.log('add-todo: click...', event);
+        let text = prompt('Todo:', 'Please, enter a todo.');
+        todos.set(todos.size + 1 + '', new Todo(todo = text));
+        setTodoList(todos);
     });
 
     document.getElementById('remove-todo').addEventListener('click', event => {
         console.log('remove-todo: click...', event);
+        todos.delete(getSelectedTodo().id);
+        clearTodoFields();
+        setTodoList(todos);
+        document.getElementById('remove-todo').disabled = true;
     });
 
     document.getElementById('todo-closed').addEventListener('change', event => {
         console.log('todo-closed: onchange...', event.target.value);
-        let id = document.getElementById('todo-id').value
-        todos.get(id).closed = event.target.value;
+        getSelectedTodo().closed = event.target.value;
     });
 
     document.getElementById('todo').addEventListener('change', event => {
         console.log('todo: onchange...', event.target.value);
-        let id = document.getElementById('todo-id').value
-        todos.get(id).todo = event.target.value;
+        getSelectedTodo().todo = event.target.value;
    });
 };
