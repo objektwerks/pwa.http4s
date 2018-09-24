b6c3de4e637064480499f1980ab923efea4d2845
objektwerks <objektwerks@runbox.com>
2018-09-24 13:03:44 -0400

    edited logging

--- pwa/service-worker-registrar.js
+++ pwa/service-worker-registrar.js
@@ -1,14 +1,14 @@
 export default () => {
     navigator.serviceWorker.
     register('/service-worker.js').
-    then(registration => console.log('service worker registered!', registration)).
-    catch(error => console.error('failed to register service worker!', error));
+    then(registration => console.log('registrar: service worker registered!', registration)).
+    catch(error => console.error('registrar: failed to register service worker!', error));
 
     navigator.serviceWorker.ready.
     then(registration => {
         registration.sync.
         register("todo-sync").
-        then(() => console.log('registered background sync.')).
-        catch(error => console.log('background sync registration failed.', error));
+        then(() => console.log('registrar: registered background sync.')).
+        catch(error => console.log('registrar: background sync registration failed.', error));
     });
 };
