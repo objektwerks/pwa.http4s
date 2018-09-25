d2d391bd7171b3c558b32344ddfc9b3a3cd42a2d
objektwerks <objektwerks@runbox.com>
2018-09-25 18:33:56 -0400

    corrected some intellij es6 errors

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -42,29 +42,29 @@ function fromStaticCache(request) {
 }
 
 self.addEventListener('install', event => {
-  console.log('install: service worker installed.', event);
-  event.waitUntil(toStaticCache());
+    console.log('install: service worker installed.', event);
+    event.waitUntil(toStaticCache());
 });
 
 self.addEventListener('activate', event => {
-  console.log('activate: service worker activated.', event);
-  return self.clients.claim();
+    console.log('activate: service worker activated.', event);
+    return self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
-    console.warn('fetch: Bug [823392] cache === only-if-cached && mode !== same-orgin', event.request);
-    return;
-  }
-  console.log('fetch: calling fromStaticCache...');
-  event.respondWith(fromStaticCache(event.request).then(response => {
-    return response || fetch(event.request);
-  }));
+    if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
+        console.warn('fetch: Bug [823392] cache === only-if-cached && mode !== same-orgin', event.request);
+        return;
+    }
+    console.log('fetch: calling fromStaticCache...');
+    event.respondWith(fromStaticCache(event.request).then(response => {
+        return response || fetch(event.request);
+    }));
 });
 
 self.addEventListener('sync', event => {
-  if (event.tag === SYNC) {
-    console.log('sync: background syncing...', event);
-    event.waitUntil(Promise.resolve);
-  }
+    if (event.tag === SYNC) {
+        console.log('sync: background syncing...', event);
+        event.waitUntil(Promise.resolve());
+    }
 });
--- pwa/todo-service.js
+++ pwa/todo-service.js
@@ -8,7 +8,7 @@ export default class TodoService {
             headers: {
                 "Content-Type": "application/json; charset=utf-8",
             }
-        }
+        };
         this.getTodosInit = Object.assign({method: "GET"}, this.todosInit);
         this.postTodoInit = Object.assign({method: "POST"}, this.todosInit);
         this.putTodoInit = Object.assign({method: "PUT"}, this.todosInit);
@@ -24,7 +24,7 @@ export default class TodoService {
             .then(arrayOfTodos => {
                 console.log('getTodos:', JSON.stringify(arrayOfTodos));
                 for (let todo of arrayOfTodos) {
-                    mapOfTodos.set(mapOfTodos.size + 1, Json.parse(todo));
+                    mapOfTodos.set(mapOfTodos.size + 1, JSON.parse(todo));
                 }
                 return mapOfTodos;
             })
@@ -43,7 +43,7 @@ export default class TodoService {
             })
             .then(Id => {
                 console.log('postTodo:', JSON.stringify(Id));
-                id = JSON.parse(Id).id;
+                id = Id.id;
             })
             .catch(error => {
                 console.error('postTodo:', error.message);
@@ -60,7 +60,7 @@ export default class TodoService {
             })
             .then(Count => {
                 console.log('putTodo:', JSON.stringify(Count));
-                count = JSON.parse(Count).count;
+                count = Count.count;
             })
             .catch(error => {
                 console.error('putTodo:', error.message);
@@ -77,7 +77,7 @@ export default class TodoService {
             })
             .then(Count => {
                 console.log('deleteodo:', JSON.stringify(Count));
-                count = JSON.parse(Count).count;
+                count = Count.count;
             })
             .catch(error => {
                 console.error('deleteTodo: ', error.message);
