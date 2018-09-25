0a099820c5191df7983d04987e16ec96caaf586b
objektwerks <objektwerks@runbox.com>
2018-09-25 19:04:34 -0400

    edited server uri

--- pwa/index.js
+++ pwa/index.js
@@ -3,6 +3,6 @@ import TodoService from './todo-service.js';
 import TodoModelView from './todo-model-view.js';
 
 registerServiceWorker();
-const todoService = new TodoService('http://127.0.0.1:7777/todos');
+const todoService = new TodoService('http://127.0.0.1:7777/api/v1/todos');
 const todoModelView = new TodoModelView(todoService);
 todoModelView.init();
