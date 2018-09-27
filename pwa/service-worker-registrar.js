e43b6c6f0293b24e580b28be4bad666b8dd69307
objektwerks <objektwerks@runbox.com>
2018-09-27 15:19:51 -0400

    added service worker registration update

--- pwa/service-worker-registrar.js
+++ pwa/service-worker-registrar.js
@@ -1,6 +1,9 @@
 export default () => {
     navigator.serviceWorker.register('/service-worker.js')
-        .then(registration => console.log('registrar: service worker registered!', registration))
+        .then(registration => {
+            registration.update();
+            console.log('registrar: service worker registered!', registration)
+        })
         .catch(error => console.error('registrar: failed to register service worker!', error));
 
     navigator.serviceWorker.ready
