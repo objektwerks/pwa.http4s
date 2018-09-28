1d5b3428f3cc545832bc0ff6deaa603d2b7b9665
objektwerks <objektwerks@runbox.com>
2018-09-28 14:57:10 -0400

    removed debug

--- pwa/todo-service.js
+++ pwa/todo-service.js
@@ -21,9 +21,7 @@ export default class TodoService {
     }
 
     postTodo(todo) {
-        console.log('postTodo: todo', JSON.stringify(todo));
         const init = Object.assign({body: JSON.stringify(todo)}, this.postTodoInit);
-        console.log('postTodo: init', init);
         return fetch(this.todosUri, init);
     }
 
